648217c0190fe7aeff2a1c3d374cd547e5c62e21
carlos briones <carlos@altas.cl>
2021-05-11 01:09:15 -0400

    documentación hecha

--- src/main/java/classes/ScrabBoolean.java
+++ src/main/java/classes/ScrabBoolean.java
@@ -1,44 +1,54 @@
 package classes;
 
-import java.util.Objects;
-
+/**
+ * Scrabble Boolean Class
+ */
 public class ScrabBoolean extends AbstractScrab{
     boolean theBool;
 
-    /** constructor of class ScrabBoolean
-     *
+    /**
+     * constructor of class ScrabBoolean
      * @param b the boolean value
      */
     public ScrabBoolean(boolean b){
         theBool=b;
     }
 
-    /** getter method for ScrabBoolean
-     *
-     * @return The Boolean value
+    /**
+     * getter method for ScrabBoolean
+     * @return the Boolean value
      */
-
      private Boolean getBool(){
         return theBool;
     }
-    /** to string for printing purposes
-     *
-     * @return the string version of the boolean
+
+    /**
+     * change the value of the ScrabBoolean
+     * @param theBool the new boolean value
+     */
+    public void setBool(boolean theBool) {
+        this.theBool = theBool;
+    }
+
+    /**
+     * to string for printing purposes
+     * @return the string version of the ScrabBoolean
      */
     @Override
     public String toString() {
         return String.valueOf(getBool());
     }
-    /** transforms Scrabble Boolean to Scrabble String
-     *
-     * @return a Scrabble String representing the Scrabble Boolean
+
+    /**
+     * transforms Scrabble Boolean to Scrabble String
+     * @return a Scrabble String representing the ScrabBoolean
      */
     public ScrabString toScrabString(){
         return new ScrabString(toString());
     }
 
-    /** check if the object is equal
-     *
+    /**
+     * check if the object is equal
      * @param o object to compare to
      * @return a boolean stating if o and this are equals
      */
